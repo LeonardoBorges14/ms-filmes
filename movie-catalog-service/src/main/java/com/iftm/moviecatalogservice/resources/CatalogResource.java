2fefbfaa5583a9b7a6dc738cb689552d07c798f3
Leonardo Borges <leonardo3ktt@gmail.com>
2021-11-20 13:45:56 -0300

    Criei um RestTemplate to call o microservice API movie

--- movie-catalog-service/src/main/java/com/iftm/moviecatalogservice/resources/CatalogResource.java
+++ movie-catalog-service/src/main/java/com/iftm/moviecatalogservice/resources/CatalogResource.java
@@ -1,23 +1,35 @@
 package com.iftm.moviecatalogservice.resources;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.iftm.moviecatalogservice.models.CatalogItem;
+import com.iftm.moviecatalogservice.models.Movie;
+import com.iftm.moviecatalogservice.models.Rating;
 
-@RestController 
+@RestController
 @RequestMapping("/catalog")
 public class CatalogResource {
-	
+
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 		
-		return Collections.singletonList(new CatalogItem("Transformers", "Filme de Robô", 4));
-				
+		RestTemplate restTemplate = new RestTemplate();
+
+		List<Rating> ratings = Arrays.asList(new Rating("12", 5), new Rating("15", 2));
+
+		return ratings.stream().map(rating -> {
+			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+			return new CatalogItem(movie.getName(), "Bom filme", rating.getRating());
+		}).collect(Collectors.toList());
+
 	}
 
 }
