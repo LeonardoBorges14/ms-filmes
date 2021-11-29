45c6316bf1f8b826a882dd73e6a90631953c1172
Leonardo Borges <leonardo3ktt@gmail.com>
2021-11-29 19:51:21 -0300

    Adicionado o Discovery Server Eureka

--- movie-info-service/src/main/java/com/iftm/movieinfoservice/resources/MovieResource.java
+++ movie-info-service/src/main/java/com/iftm/movieinfoservice/resources/MovieResource.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.iftm.movieinfoservice.model.Movie;
+import com.iftm.movieinfoservice.models.Movie;
 
 @RestController 
 @RequestMapping("/movies")
